4bae73034eb9bb0dfc495d1f354d6f6f98f5755d
Terry Miller <tdmiller@questra.org>
2011-10-26 13:37:15 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'b17cda823f6a27ab9270dfac36748c80b3b03c03ae6bbe8fcab25f99ab37aed36d1210de7fc71a91d8e15ee7af892e87ccae4d0f7d85216a17abd806c42027b5'
